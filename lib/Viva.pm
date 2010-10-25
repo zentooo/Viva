9296c736b8b50b11ae55aa83ea03872138e75c5e
zentooo <ankerasoy@gmail.com>
2010-10-25 23:50:58 +0900

    initial commit

---
+++ lib/Viva.pm
@@ -0,0 +1,32 @@
+package Viva;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Viva -
+
+=head1 SYNOPSIS
+
+  use Viva;
+
+=head1 DESCRIPTION
+
+Viva is
+
+=head1 AUTHOR
+
+zentooo E<lt>@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
